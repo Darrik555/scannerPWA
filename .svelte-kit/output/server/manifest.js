6cad4eb85b7337624de1f61fb59dcf839bda0b3d
Denis Rotmann <denis.rotmann@inform-software.com>
2025-04-01 12:13:52 +0200

    revert

---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,29 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png","icon192.png","icon512.png","manifest.json"]),
+	mimeTypes: {".png":"image/png",".json":"application/json"},
+	_: {
+		client: {start:"_app/immutable/entry/start.TzNtTYSq.js",app:"_app/immutable/entry/app.CfXjdZU1.js",imports:["_app/immutable/entry/start.TzNtTYSq.js","_app/immutable/chunks/CcLRmp2I.js","_app/immutable/chunks/oLtghWby.js","_app/immutable/chunks/CV5EsZTn.js","_app/immutable/entry/app.CfXjdZU1.js","_app/immutable/chunks/oLtghWby.js","_app/immutable/chunks/CPDYnXr9.js","_app/immutable/chunks/DOq4m96K.js","_app/immutable/chunks/CowqXRAS.js","_app/immutable/chunks/B6zllX-l.js","_app/immutable/chunks/CV5EsZTn.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js'))
+		],
+		routes: [
+			
+		],
+		prerendered_routes: new Set(["/"]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.DaWZGGAI.js","_app/immutable/chunks/CowqXRAS.js","_app/immutable/chunks/oLtghWby.js","_app/immutable/chunks/rQ8KRRup.js","_app/immutable/chunks/CPDYnXr9.js","_app/immutable/chunks/DOq4m96K.js","_app/immutable/chunks/CcLRmp2I.js","_app/immutable/chunks/CV5EsZTn.js"];
+export const stylesheets = [];
+export const fonts = [];
