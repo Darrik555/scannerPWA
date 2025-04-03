ab76b80a98d96b919e4b8ff0dab72fd05d288cb8
Denis Rotmann <denis.rotmann@inform-software.com>
2025-04-03 13:30:00 +0200

    fix

--- scanner-pwa/src/lib/camera.ts
+++ scanner-pwa/src/lib/camera.ts
@@ -20,7 +20,6 @@ export async function start(videoElement: HTMLVideoElement){
 }
 
 export function stop(videoElement: HTMLVideoElement, stream: MediaStream) {
-    videoElement.pause();
     videoElement.removeAttribute('srcObject');
     videoElement.removeAttribute('src');
     videoElement.load();
