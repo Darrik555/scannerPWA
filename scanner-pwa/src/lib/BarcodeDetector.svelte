d9bbf0dd84f470c3d8e25d11f8534bd7ecbbcdfc
Denis Rotmann <denis.rotmann@inform-software.com>
2025-04-04 14:53:39 +0200

    canvas

--- scanner-pwa/src/lib/BarcodeDetector.svelte
+++ scanner-pwa/src/lib/BarcodeDetector.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { type DetectedBarcode } from "barcode-detector/ponyfill";
   import { scanBarcode } from "$lib/scanner";
   import { onDestroy, onMount } from "svelte";
   import * as camaraController from "$lib/camera";
 
-  let video: HTMLVideoElement = $props();
+  let video: HTMLVideoElement;
   let stream: MediaStream;
+  let boundingBox: HTMLCanvasElement;
   let isScanning = $state(false);
   let cameraActive = $state(false);
   let isMounted = $state(false);
@@ -17,7 +19,11 @@
     camaraController.stop(video, stream);
   }
   function startScanner() {
-    camaraController.stop(video, stream);
+    camaraController.start(video);
+  }
+
+  function onLocate(detectedCodes: DetectedBarcode[]) {
+    const width = video.offsetWidth;
   }
 
   onMount(() => {
@@ -39,13 +45,17 @@
 </script>
 
 <div>
-  <video
-    bind:this={video}
-    onloadeddata={handleScanning}
-    muted
-    autoplay
-    playsinline
-  ></video>
+  <div>
+    <video
+      bind:this={video}
+      onloadeddata={handleScanning}
+      muted
+      autoplay
+      playsinline
+    ></video>
+    <canvas id="code-bounding-box" bind:this={boundingBox}> </canvas>
+  </div>
+
   <input type="text" bind:value={barcodeValue} />
   <button onclick={startScanner}>Start</button>
   <button onclick={stopScanner}>Stop</button>
@@ -54,7 +64,16 @@
 <style>
   video {
     width: 100%;
+    height: 100%;
     max-width: 400px;
     border: 2px solid #000;
+  }
+
+  #code-bounding-box {
+    width: 100%;
+    height: 100%;
+    left: 0px;
+    top: 0px;
+    color: #123;
   }
 </style>
