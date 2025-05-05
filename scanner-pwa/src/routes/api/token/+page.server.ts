bcf0e8aebdadc9e210911b31c84a395fa0c685fe
Denis Rotmann <denis.rotmann@inform-software.com>
2025-05-05 12:50:30 +0200

    cors fix

--- scanner-pwa/src/routes/api/token/+page.server.ts
+++ scanner-pwa/src/routes/api/token/+page.server.ts
@@ -3,24 +3,42 @@ import jwt from 'jsonwebtoken';
 // DONT USE THIS CODE
 const SECRET = 'secret';
 
+const corsHeaders = {
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Headers':'*',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS',
+    'Content-Type':'application/json'
+};
+
+
+
+
 export const actions: RequestHandler = async({url}) => {
-    const role = url.searchParams.get('role');
+    try{
+        const role = url.searchParams.get('role');
 
-    if(!role){
-        return new Response(JSON.stringify({error: 'Missing role'}),
-    { 
-        headers:{
-        'Access-Control-Allow-Origin':'*',
-        'Content-Type':'application/json'},
-    });
+        if(!role){
+            return new Response(JSON.stringify({
+                    error: 'Missing role'}),
+                { 
+                    status: 400,
+                    headers: corsHeaders
+                });
+        }
+    
+        const token = jwt.sign({role},SECRET,{expiresIn: '20m'})
+    
+        return new Response(JSON.stringify({token}), 
+        { 
+            status: 200,
+            headers:corsHeaders
+        });
+    }catch(e){
+        return new Response(JSON.stringify({error:'Server errror'}), 
+        { 
+            status: 500,
+            headers:corsHeaders
+        });  
     }
-
-    const token = jwt.sign({role},SECRET,{expiresIn: '20m'})
-
-    return new Response(JSON.stringify({token}), 
-    { 
-        headers:{
-        'Access-Control-Allow-Origin':'*',
-        'Content-Type':'application/json'},
-    });
+   
 };
