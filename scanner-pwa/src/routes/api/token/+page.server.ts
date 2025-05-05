b86fd0f19c5ed50a14992ed34cc1d47b2eee3818
Denis Rotmann <denis.rotmann@inform-software.com>
2025-05-05 12:17:06 +0200

    cors

--- scanner-pwa/src/routes/api/token/+page.server.ts
+++ scanner-pwa/src/routes/api/token/+page.server.ts
@@ -1,16 +1,26 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
-
+// DONT USE THIS CODE
 const SECRET = 'secret';
 
 export const actions: RequestHandler = async({url}) => {
     const role = url.searchParams.get('role');
 
     if(!role){
-        return new Response(JSON.stringify({error: 'Missing role'}),{status: 400});
+        return new Response(JSON.stringify({error: 'Missing role'}),
+    { 
+        headers:{
+        'Access-Control-Allow-Origin':'*',
+        'Content-Type':'application/json'},
+    });
     }
 
     const token = jwt.sign({role},SECRET,{expiresIn: '20m'})
 
-    return new Response(JSON.stringify({token}), { headers:{'Content-Type':'application/json'},});
+    return new Response(JSON.stringify({token}), 
+    { 
+        headers:{
+        'Access-Control-Allow-Origin':'*',
+        'Content-Type':'application/json'},
+    });
 };
