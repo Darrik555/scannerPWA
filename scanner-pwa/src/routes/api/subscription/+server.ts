5f2c57172bc60925c11634f338261d12733abafc
Denis Rotmann <denis.rotmann@inform-software.com>
2025-05-09 18:02:32 +0200

    console.log

--- scanner-pwa/src/routes/api/subscription/+server.ts
+++ scanner-pwa/src/routes/api/subscription/+server.ts
@@ -4,9 +4,15 @@ import type { PushSubscription } from "web-push";
 const subscriptions: PushSubscription[] = [];
 
 export const POST: RequestHandler = async({request}) => {
-    const subscription = await request.json();
-    subscription.push(subscription);
-    return new Response('Subscription saved');
+    try{
+        const subscription = await request.json();
+        console.log("received subscription"+ subscription + process.env.PRIVATE_VAPID_KEY);
+        subscription.push(subscription);
+        return new Response('Subscription saved');
+    }catch(e){
+        console.error("Subscription error: ", e);
+        return new Response("Server error");
+    }
 }
 
 export function _getAllSubscriptions(){
