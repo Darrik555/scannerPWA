5f4b070fb57234ac8d8c64238b0c7600d893cfa2
Denis Rotmann <denis.rotmann@inform-software.com>
2025-05-09 15:16:10 +0200

    moved code out of sw

--- scanner-pwa/src/lib/pushNotificationController.ts
+++ scanner-pwa/src/lib/pushNotificationController.ts
@@ -1,15 +1,53 @@
 
+let registration: ServiceWorkerRegistration;
+
+function urlB64ToUint8Array(base64String: string) {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+}
+
+async function saveSubscription(subscription: PushSubscription) {
+    const API_PATH = "/api/subscription";
+    const response = await fetch(API_PATH, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(subscription)
+    });
+    return response.json();
+} 
+
 export async function requestNotificationPermission() {
+
 
     if (!('serviceWorker' in navigator)) {
         throw new Error('No Service Worker support!');
       }
-      if (!('PushManager' in window)) {
+    if (!('PushManager' in window)) {
         throw new Error('No Push API Support!');
-      }
+    }
 
     const permission = await window.Notification.requestPermission();
     if (permission !== 'granted') {
       throw new Error('Permission not granted for Notification');
+    }
+
+    try{
+        registration = await navigator.serviceWorker.register('/service-worker.ts');
+        const applicationServerKey = urlB64ToUint8Array(
+            "BIuD3JFVLjCFT3WDZ3fbh7bCW-XFaNCa7woQVp8Q6rfPBoIE6JVxW2U7fkUJGMfJe8EFUlF4AuQwL63N5eJzIr8"
+        );
+        const options = {applicationServerKey, userVisibleOnly: true};
+        const subscription = await registration.pushManager.subscribe(options);
+        saveSubscription(subscription);
+    }catch(e){
+        console.error("Push Registration failed");
     }
 }
--- scanner-pwa/src/service-worker.ts
+++ scanner-pwa/src/service-worker.ts
@@ -11,46 +11,12 @@ const ASSETS = [
 	...files  // everything in `static`
 ];
 
-function urlB64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
-    const rawData = atob(base64)
-    const outputArray = new Uint8Array(rawData.length)
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i)
-    }
-    return outputArray
-}
-
-async function saveSubscription(subscription) {
-    const API_PATH = "/api/subscription";
-    const response = await fetch(API_PATH, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(subscription)
-    });
-    return response.json();
-} 
-
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
 	// Create a new cache and add all files to it
 	async function addFilesToCache() {
 		const cache = await caches.open(CACHE);
 		await cache.addAll(ASSETS);
 	}
-
-    try{
-        const applicationServerKey = urlB64ToUint8Array(
-            "BIuD3JFVLjCFT3WDZ3fbh7bCW-XFaNCa7woQVp8Q6rfPBoIE6JVxW2U7fkUJGMfJe8EFUlF4AuQwL63N5eJzIr8"
-        );
-        const options = {applicationServerKey, userVisibleOnly: true};
-        const subscription = await self.ServiceWorkerRegistration.pushManager.subscribe(options);
-        const response = await saveSubscription(subscription);
-    }catch(e){
-        console.error("Error", e);
-    }
 
 	event.waitUntil(addFilesToCache());
 });
@@ -64,14 +30,6 @@ self.addEventListener('activate', async (event) => {
 	}
 
 	event.waitUntil(deleteOldCaches());
-
-    try {
-        const options = {}
-        const subscription = await self.registration.pushManager.subscribe(options)
-        console.log(JSON.stringify(subscription))
-      } catch (err) {
-        console.log('Error', err)
-      }
 });
 
 self.addEventListener('fetch', (event) => {
