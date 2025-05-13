59879c347e8135bad53122a51bc3016e026e2dee
Denis Rotmann <denis.rotmann@inform-software.com>
2025-05-13 15:19:39 +0200

    add map page

---
+++ scanner-pwa/src/routes/map/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import L from "leaflet";
+  import "leaflet/dist/leaflet.css";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const userPosition = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+
+      const map = L.map("map").setView(
+        [userPosition.latitude, userPosition.longitude],
+        5
+      );
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+        maxZoom: 20,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+      }).addTo(map);
+
+      L.marker([userPosition.latitude, userPosition.longitude]).addTo(map);
+    });
+  });
+</script>
+
+<h1>Map</h1>
+<div id="map"></div>
+
+<style>
+  #map {
+    position: absolute;
+    bottom: 0;
+    height: 50%;
+    width: 100%;
+  }
+</style>
