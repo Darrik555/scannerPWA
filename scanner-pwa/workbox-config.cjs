1ed60c6133f87eb3240b094f1bfb93c699a27217
Denis Rotmann <denis.rotmann@inform-software.com>
2025-06-06 11:17:46 +0200

    Revert "workbox test"
    This reverts commit 932b9e68b5940339b144af503a4c794a7d5252fe.

--- scanner-pwa/workbox-config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globDirectory: '.svelte-kit/output/client',
-	globPatterns: [
-		'**/*.{html,js,png}'
-	],
-	swDest: 'build/service-worker.js',
-	swSrc: 'src/service-worker.js'
-};
