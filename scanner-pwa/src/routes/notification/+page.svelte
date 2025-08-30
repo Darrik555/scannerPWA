e0f8d7919cb41969e8efb0880bc2e92834564005
Darrik555 <you@example.com>
2025-08-30 21:27:31 +0200

    move notification to button

--- scanner-pwa/src/routes/notification/+page.svelte
+++ scanner-pwa/src/routes/notification/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { requestNotificationPermission } from "$lib/pushNotificationController";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    requestNotificationPermission();
-  });
 </script>
 
 <h1>Notifications</h1>
+
+<button on:click={requestNotificationPermission}
+  >Request Permission and Subscribe</button
+>
