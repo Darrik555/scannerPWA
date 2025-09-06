a00c426071eed79e1b1a9f0f14e846fd4a767c94
Darrik555 <you@example.com>
2025-09-06 14:27:38 +0200

    Revert "Reapply "trigger sw update""
    This reverts commit 92a392faf1056f0b3567582da27df133681ae767.

--- scanner-pwa/src/lib/BarcodeDetector.svelte
+++ scanner-pwa/src/lib/BarcodeDetector.svelte
@@ -27,7 +27,6 @@
       let startTimeScanning = performance.now();
       const barcode = await scanBarcode(video);
       let endTimeScanning = performance.now();
-      //time
       timeScan = endTimeScanning - startTimeScanning;
 
       barcodeValue = barcode ?? "";
